2848fa976e0e8a3ddee62bb426a3a7e76faa806a
tristanfisher <code@tristanfisher.com>
2014-03-09 16:40:44 -0400

    removes init

--- newsworm/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-
-db = SQLAlchemy(app)
